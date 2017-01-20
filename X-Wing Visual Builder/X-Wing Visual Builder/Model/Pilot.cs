0dc92bcf631b36477a26ee8b64958b81776692a6
Falesh <web@johnbaldock.com>
2017-01-20 00:02:58 +0000

    Initial building

---
+++ X-Wing Visual Builder/X-Wing Visual Builder/Model/Pilot.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace X_Wing_Visual_Builder.Model
+{
+    class Pilot
+    {
+        private Faction faction;
+        private UpgradeType[] upgrades;
+        private Action[] actions;
+
+        // TODO add where you can buy this card
+        // TODO add maneuver card array[5][9] maneuver[4][0] = Maneuver.Green is a green 4 turn left, maneuver[5][8] = Maneuver.Red is a red 5 K turn    
+
+        private byte pilotSkill;
+        private byte primaryWeaponValue;
+        private byte agility;
+        private byte hull;
+        private byte shields;
+        private byte cost;
+
+        private string pilotAbility;
+
+        public void SetFaction(Faction newFaction)
+        {
+            faction = newFaction;
+        }
+    }
+}
---
+++ X-Wing Visual Builder/X-Wing Visual Builder/Model/enums.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace X_Wing_Visual_Builder.Model
+{
+    enum Faction { RebelAlliance, Resistance, GalacticEmpire, FirstOrder, ScumAndVillainy }
+    enum UpgradeType { Astromech, Bomb, Cannon, Cargo, Crew, Elite, Hardpoint, Illicit, Missiles, Modification, SalvagedAstromech, System, Team, Tech, Title, Torpedoes, Turret }
+    enum Action { BarrelRoll, Boost, Cloak, Coordinate, Evade, Focus, Jam, Recover, Reinforce, RotateArc, SLAM, TargetLock }
+    enum Maneuver { White, Green, Red }
+}
